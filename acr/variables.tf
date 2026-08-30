630c4b76bed32c1b04aa6ce52d11ba4d9bc08996
clouddevopswithkrishna <clouddevopswithkrishna@gmail.com>
2026-08-30 18:10:32 +0530

    Update variables.tf

--- acr/variables.tf
+++ acr/variables.tf
@@ -1,32 +1,32 @@
 variable "name" {
-  description = "Name of the Azure Container Registry (must be globally unique, alphanumeric)."
+  description = "Name of the Azure Container Registry (globally unique, alphanumeric only)"
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "Resource group in which to create the registry."
+  description = "Name of the Resource Group where the ACR will be created"
   type        = string
 }
 
 variable "location" {
-  description = "Azure region."
+  description = "Azure region where the ACR will be created"
   type        = string
 }
 
 variable "sku" {
-  description = "ACR SKU: Basic, Standard, or Premium."
+  description = "ACR SKU: Basic, Standard, or Premium"
   type        = string
   default     = "Standard"
 }
 
 variable "admin_enabled" {
-  description = "Enable the ACR admin account (not recommended for production; prefer AcrPull role assignment)."
+  description = "Enable the ACR admin account (username/password)"
   type        = bool
   default     = false
 }
 
 variable "tags" {
-  description = "Tags to apply to the registry."
+  description = "Tags to apply to the ACR"
   type        = map(string)
   default     = {}
 }
