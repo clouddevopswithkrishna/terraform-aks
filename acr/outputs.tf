66bbaeb729026c27fa13f427e9665073cf754be5
clouddevopswithkrishna <clouddevopswithkrishna@gmail.com>
2026-08-30 18:10:42 +0530

    Update outputs.tf

--- acr/outputs.tf
+++ acr/outputs.tf
@@ -1,9 +1,14 @@
 output "id" {
-  description = "Resource ID of the container registry."
+  description = "Resource ID of the ACR"
   value       = azurerm_container_registry.this.id
 }
 
+output "name" {
+  description = "Name of the ACR"
+  value       = azurerm_container_registry.this.name
+}
+
 output "login_server" {
-  description = "Login server hostname of the registry."
+  description = "Login server hostname of the ACR (e.g. myacr.azurecr.io)"
   value       = azurerm_container_registry.this.login_server
 }
