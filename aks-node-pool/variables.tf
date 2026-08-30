862adacea8910e7003b37005edecc3041c1b0420
clouddevopswithkrishna <clouddevopswithkrishna@gmail.com>
2026-08-30 18:08:35 +0530

    Update variables.tf

--- aks-node-pool/variables.tf
+++ aks-node-pool/variables.tf
@@ -1,97 +1,88 @@
-variable "name" {
-  description = "Name of the node pool (max 12 chars, alphanumeric)."
+# =============================================================================
+# AKS Node Pool Module - Variables
+# =============================================================================
+
+variable "cluster_id" {
+  description = "Resource ID of the AKS cluster (from the aks module) to attach these node pools to"
   type        = string
 }
 
-variable "aks_cluster_id" {
-  description = "Resource ID of the AKS cluster to attach this node pool to."
+variable "default_subnet_id" {
+  description = "Default subnet ID to use for node pools that don't specify their own subnet_id"
   type        = string
 }
 
-variable "subnet_id" {
-  description = "Subnet ID for this node pool."
+variable "default_kubernetes_version" {
+  description = "Default Kubernetes version applied to node pools that don't override kubernetes_version. Normally set to the same value as the cluster's kubernetes_version."
   type        = string
 }
 
-variable "vm_size" {
-  description = "VM SKU for nodes in this pool."
-  type        = string
-}
+variable "node_pools" {
+  description = <<-EOT
+    Map of user node pools to create. Key = pool name, Value = pool configuration.
+    Adding, removing, resizing, or upgrading a pool is done purely by editing
+    this map in terraform.tfvars - no module or code changes required.
 
-variable "orchestrator_version" {
-  description = "Kubernetes version for this node pool. Defaults to the cluster version when null."
-  type        = string
-  default     = null
-}
-
-variable "os_type" {
-  description = "Linux or Windows."
-  type        = string
-  default     = "Linux"
-}
-
-variable "os_disk_size_gb" {
-  description = "OS disk size in GB."
-  type        = number
-  default     = 128
-}
-
-variable "mode" {
-  description = "System or User node pool mode."
-  type        = string
-  default     = "User"
-}
-
-variable "max_pods" {
-  description = "Maximum pods per node."
-  type        = number
-  default     = 30
-}
-
-variable "availability_zones" {
-  description = "Availability zones for this node pool."
-  type        = list(string)
-  default     = []
-}
-
-variable "enable_auto_scaling" {
-  description = "Enable the cluster autoscaler for this node pool."
-  type        = bool
-  default     = true
-}
-
-variable "node_count" {
-  description = "Fixed node count when autoscaling is disabled."
-  type        = number
-  default     = 1
-}
-
-variable "min_count" {
-  description = "Minimum node count when autoscaling is enabled."
-  type        = number
-  default     = 1
-}
-
-variable "max_count" {
-  description = "Maximum node count when autoscaling is enabled."
-  type        = number
-  default     = 3
-}
-
-variable "node_labels" {
-  description = "Kubernetes node labels."
-  type        = map(string)
-  default     = {}
-}
-
-variable "node_taints" {
-  description = "Kubernetes node taints, e.g. [\"workload=batch:NoSchedule\"]."
-  type        = list(string)
-  default     = []
+    Example:
+    node_pools = {
+      "userpool1" = {
+        vm_size              = "Standard_D4s_v5"
+        mode                 = "User"
+        os_type              = "Linux"
+        enable_auto_scaling  = true
+        node_count           = 3
+        min_count            = 2
+        max_count            = 10
+        max_pods             = 30
+        zones                = ["1", "2", "3"]
+        priority             = "Regular"
+        node_labels          = { workload = "general" }
+        node_taints          = []
+      }
+      "spotpool1" = {
+        vm_size              = "Standard_D4s_v5"
+        mode                 = "User"
+        os_type              = "Linux"
+        enable_auto_scaling  = true
+        node_count           = 0
+        min_count            = 0
+        max_count            = 5
+        max_pods             = 30
+        priority             = "Spot"
+        eviction_policy      = "Delete"
+        spot_max_price       = -1
+        node_labels          = { workload = "batch" }
+        node_taints          = ["kubernetes.azure.com/scalesetpriority=spot:NoSchedule"]
+      }
+    }
+  EOT
+  type = map(object({
+    vm_size              = string
+    subnet_id             = optional(string, null)
+    kubernetes_version   = optional(string, null)
+    mode                  = optional(string, "User")
+    os_type               = optional(string, "Linux")
+    os_disk_size_gb       = optional(number, 128)
+    os_disk_type          = optional(string, "Managed")
+    zones                 = optional(list(string), [])
+    enable_auto_scaling  = optional(bool, true)
+    node_count            = optional(number, 1)
+    min_count             = optional(number, 1)
+    max_count              = optional(number, 3)
+    max_pods               = optional(number, 30)
+    node_labels           = optional(map(string), {})
+    node_taints           = optional(list(string), [])
+    priority              = optional(string, "Regular")
+    eviction_policy       = optional(string, null)
+    spot_max_price         = optional(number, null)
+    upgrade_max_surge     = optional(string, "33%")
+    tags                  = optional(map(string), {})
+  }))
+  default = {}
 }
 
 variable "tags" {
-  description = "Tags to apply to the node pool."
+  description = "A map of tags to apply to all node pools (merged with any per-pool tags)"
   type        = map(string)
   default     = {}
 }
