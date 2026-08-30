bfa5efcc06ab4b496868768c25728651641f3dc1
clouddevopswithkrishna <clouddevopswithkrishna@gmail.com>
2026-08-30 18:58:59 +0530

    Update main.tf

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -3,7 +3,7 @@
 ############################################
 
 resource "azurerm_resource_group" "this" {
-  name     =  var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
   tags     = var.tags
 }
