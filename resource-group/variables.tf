8b72e8c63907ea94bf20a9eb86206a177f7859fd
clouddevopswithkrishna <clouddevopswithkrishna@gmail.com>
2026-08-30 18:00:29 +0530

    Update variables.tf

--- resource-group/variables.tf
+++ resource-group/variables.tf
@@ -1,15 +1,24 @@
-variable "name" {
-  description = "Name of the resource group."
+# =============================================================================
+# Resource Group Module - Variables
+# =============================================================================
+
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group to create"
   type        = string
+
+  validation {
+    condition     = length(var.resource_group_name) > 0 && length(var.resource_group_name) <= 90
+    error_message = "resource_group_name must be between 1 and 90 characters."
+  }
 }
 
 variable "location" {
-  description = "Azure region for the resource group."
+  description = "Azure region where the Resource Group will be created (e.g. eastus, westeurope)"
   type        = string
 }
 
 variable "tags" {
-  description = "Tags to apply to the resource group."
+  description = "A map of tags to apply to the Resource Group"
   type        = map(string)
   default     = {}
 }
